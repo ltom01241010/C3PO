59867c0603decb133284d18b49490fe3d48153d3
Zhongyang Li <65762714+ltom01241010@users.noreply.github.com>
2025-03-29 07:16:52 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,13 +7,8 @@ git lfs install
 mkdir -p reference_data
 
 # Clone datasets from Hugging Face
-echo "Downloading SciQ dataset..."
 git clone https://huggingface.co/datasets/allenai/sciq
-
-echo "Downloading OpenBookQA dataset..."
 git clone https://huggingface.co/datasets/allenai/openbookqa
-
-echo "Downloading ARC dataset..."
 git clone https://huggingface.co/datasets/allenai/ai2_arc
 
 echo "All datasets downloaded successfully!"
